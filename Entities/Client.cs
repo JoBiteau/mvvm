34b2199510aa6a0107c68b28edd92e2d06f08f9d
Jo <joffrey.biteau@viacesi.fr>
2020-03-17 14:52:10 +0100

    Ex1

---
+++ Entities/Client.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvvmTD.Entities
+{
+    public class Client : Personne
+    {
+    }
+}
---
+++ Entities/Personne.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvvmTD
+{
+    abstract class Personne
+    {
+        private string nom;
+        private string prenom;
+        private string email;
+        private string adresse;
+    }
+}
